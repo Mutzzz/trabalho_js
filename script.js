73a31a5e48e34972a29cc63db3d075028e2cd1c9
Gálatas Mutz <galatasmutz@gmail.com>
2023-06-12 18:26:05 -0300

    Erro

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+
+
+let valorA = parseInt(document.getElementById('numeroA').value)
+
+let valorB = parseInt(document.getElementById('numeroB').value)
+
+
+function maximo(valorA, valorB){
+    if (valorA > valorB)
+        return valorA;
+    else return valorB;
+}
+let resultado = maximo (parseInt(valorA, valorB).value)
+    alert('o resultado foi ' + resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
